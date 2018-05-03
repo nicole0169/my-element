a7cbe164a5c653fa6850d27c0954a74c44d06949
miaomin <miaomin@bitmap3d.com.cn>
2018-05-03 16:14:32 +0800

    My first vue app.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h2>Essential Links</h2>
+        <el-autocomplete
+                class="inline-input"
+                v-model="input"
+                :fetch-suggestions="querySearch"
+                placeholder="City name"
+                :trigger-on-focus="false"
+                @select="handleSelect"
+        ></el-autocomplete>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                input: '',
+                cities: []
+            };
+        },
+        methods: {
+            querySearch(queryString, cb){
+                var cities = this.cities;
+                var results = queryString ? cities.filter(this.createFilter(queryString)) : cities;
+                //cb
+                cb(results);
+            },
+            createFilter(queryString){
+                return (city)=>{
+                    return (city.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+                };
+            },
+            loadAll(){
+
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
