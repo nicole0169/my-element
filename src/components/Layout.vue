1a63a663c189d59be564b29a678ad43c30a34fef
miaomin <miaomin@bitmap3d.com.cn>
2018-05-14 16:51:23 +0800

    add container layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -4,15 +4,15 @@
         <h2>Layout Component</h2>
         <el-container style="height: 500px; border: 1px solid #eeeeee">
             <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-                <el-menu :default-openeds="['1', '3']">
+                <el-menu router :default-active="$route.path" class="el-menu-vertical-demo">
                     <el-submenu index="1">
                         <template slot="title"><i class="el-icon-message"></i>导航一</template>
                         <el-menu-item-group>
                             <template slot="title">分组一</template>
-                            <el-menu-item index="1-1">选项1</el-menu-item>
-                            <el-menu-item index="1-2">选项2</el-menu-item>
+                            <el-menu-item index="/1-1">选项1</el-menu-item>
+                            <el-menu-item index="/1-2">选项2</el-menu-item>
                         </el-menu-item-group>
-                        <el-menu-item-group title="分组2">
+                        <el-menu-item-group title="分组二">
                             <el-menu-item index="1-3">选项3</el-menu-item>
                         </el-menu-item-group>
                         <el-submenu index="1-4">
@@ -55,5 +55,9 @@
 
     a {
         color: #42b983;
+    }
+
+    .el-aside {
+        color: #333;
     }
 </style>
